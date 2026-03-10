93efc622de3c35d22374d8eeae424fb7863cefdb
parthvijay123 <parth22vijay22@gmail.com>
2026-03-10 21:02:48 +0530

    add module declarations for render build

--- server/src/types.d.ts
+++ server/src/types.d.ts
@@ -1,3 +1,12 @@
 declare module "uuid";
 declare module "express";
 declare module "cors";
+
+
+import "express-serve-static-core"
+
+declare module "express-serve-static-core" {
+  interface Request {
+    params: any
+  }
+}
